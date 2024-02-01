3e45b6b5417d16732efc3069e8513936caba1c39
unknown <fairuuz.dzakiy@gmail.com>
2024-02-01 13:04:26 +0700

    kasir

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Http\Controllers\AdminKategoriController;
+use App\Http\Controllers\AdminUserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+Route::get('/', function () {
+    $data = [
+        'content' => 'admin.dashboard.index'
+    ];
+    return view('admin.layouts.wrapper', $data);
+});
+
+Route::prefix('/admin')->group(function () {
+    Route::resource('/kategori', AdminKategoriController::class);
+    Route::resource('/user', AdminUserController::class);
+});
